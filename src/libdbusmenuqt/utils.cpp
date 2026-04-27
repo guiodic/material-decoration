eaff83e1bca961f0dcf36d2696deb0af0420ecd9
Guido Iodice <guido.iodice@gmail.com>
2026-04-27 19:52:22 +0200

    fixmissing underscores in menu actions via an heuristic; also use KLocalizedString::removeAcceleratorMarker() to manage ampersands in AppMenuButtonGroup

--- src/libdbusmenuqt/utils.cpp
+++ src/libdbusmenuqt/utils.cpp
@@ -19,20 +19,28 @@ QString swapMnemonicChar(const QString &in, const char src, const char dst)
         QChar ch = in[pos];
         if (ch == src) {
             if (pos == in.length() - 1) {
-                // 'src' at the end of string, skip it
+                // 'src' at the end of string, keep it
+                out += src;
                 ++pos;
+            } else if (in[pos + 1] == src) {
+                // A real 'src'
+                out += src;
+                pos += 2;
             } else {
-                if (in[pos + 1] == src) {
-                    // A real 'src'
-                    out += src;
-                    pos += 2;
-                } else if (!mnemonicFound) {
-                    // We found the mnemonic
+                bool isMnemonic = !mnemonicFound;
+
+                // Heuristic: if followed by a digit and preceded by an alphanumeric char,
+                // it's probably part of an identifier (like video_1) rather than a mnemonic.
+                if (isMnemonic && in[pos + 1].isDigit() && pos > 0 && in[pos - 1].isLetterOrNumber()) {
+                    isMnemonic = false;
+                }
+
+                if (isMnemonic) {
                     mnemonicFound = true;
                     out += dst;
                     ++pos;
                 } else {
-                    // We already have a mnemonic, just skip the char
+                    out += src;
                     ++pos;
                 }
             }
